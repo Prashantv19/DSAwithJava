09cc833f8c93e246755f4d287f02285e2590e35d
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-04-07 23:52:54 +0530

    commit today

--- src/Arrays/twoSum.java
+++ src/Arrays/twoSum.java
@@ -3,7 +3,7 @@ package Arrays;
 public class twoSum {
     public static void main(String[] args) {
         int[] arr = {3,-1,8,5,4,9,2};
-        int x = 9;
+        int x = 10;
         int n = arr.length;
         for (int i = 0; i <n; i++) {
             for (int j = i+1; j <n; j++) {
